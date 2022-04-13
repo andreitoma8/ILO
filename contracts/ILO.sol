4317fcb8718c7b3afe3fae33f9d5ce665dc52bdd
andreitoma8 <andrei.toma8@yahoo.com>
2022-04-13 12:11:26 +0300

    main

--- contracts/ILO.sol
+++ contracts/ILO.sol
@@ -3,17 +3,21 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract ILO {
+contract ILO is Ownable {
     IERC20 token;
 
     bool public iloFulfilled = false;
 
-    bool public iloSucces;
+    bool public iloSuccess;
 
     uint256 public tokensSold;
 
     uint256 public fundsRaised;
+
+    uint256 public immutable minBuy;
 
     uint256 internal immutable totalSaleSupply;
 
@@ -29,6 +33,8 @@ contract ILO {
 
     uint256 public immutable pricePerTokenPresale;
 
+    bytes32 internal immutable merkleRoot;
+
     mapping(address => uint256) tokenBougth;
 
     mapping(address => uint256) valuePaid;
@@ -42,7 +48,9 @@ contract ILO {
         uint256 _whitelistSaleStart,
         uint256 _pricePerToken,
         uint256 _pricePerTokenPresale,
-        uint256 _tokenAmountToSale
+        uint256 _tokenAmountToSale,
+        uint256 _minBuy,
+        bytes32 _merkleRoot
     ) {
         token = IERC20(_token);
         whitelistSaleStart = _whitelistSaleStart;
@@ -52,15 +60,25 @@ contract ILO {
         pricePerTokenPresale = _pricePerTokenPresale;
         token.transferFrom(msg.sender, address(this), _tokenAmountToSale);
         totalSaleSupply = _tokenAmountToSale;
+        minBuy = _minBuy;
+        merkleRoot = _merkleRoot;
     }
 
-    function whitelistSaleBuy() public payable {
+    function whitelistSaleBuy(bytes32[] calldata _merkleProof) public payable {
         require(
             block.timestamp > whitelistSaleStart &&
                 block.timestamp < mainSaleStart,
             "Whitelist Sale not active"
         );
-        require(msg.value > 0, "You have sent no value.");
+        require(
+            msg.value > minBuy,
+            "Your transaction value is below minimum buy"
+        );
+        bytes32 leaf = keccak256(abi.encodePacked((msg.sender)));
+        require(
+            MerkleProof.verify(_merkleProof, merkleRoot, leaf),
+            "Invalid proof"
+        );
         valuePaid[msg.sender] += msg.value;
         tokenBougth[msg.sender] += msg.value / pricePerTokenPresale;
         tokensSold += msg.value / pricePerTokenPresale;
@@ -73,7 +91,10 @@ contract ILO {
             block.timestamp > mainSaleStart && block.timestamp < mainSaleEnd,
             "Whitelist Sale didn't start yet"
         );
-        require(msg.value > 0, "You have sent no value.");
+        require(
+            msg.value > minBuy,
+            "Your transaction value is below minimum buy"
+        );
         valuePaid[msg.sender] += msg.value;
         tokenBougth[msg.sender] += msg.value / pricePerToken;
         tokensSold += msg.value / pricePerToken;
@@ -102,12 +123,21 @@ contract ILO {
             uint256 _amountToSend = valuePaid[msg.sender];
             tokenBougth[msg.sender] = 0;
             valuePaid[msg.sender] = 0;
-            token.transfer(msg.sender, _amountToSend);
+            (bool os, ) = payable(msg.sender).call{value: _amountToSend}("");
+            require(os);
             emit Withdrawal(msg.sender, _amountToSend);
         }
     }
 
     function depositLiquidity() internal {}
 
-    function withdrawOwner() public {}
+    function withdrawOwner() public onlyOwner {
+        require(iloFulfilled, "ILO is not over");
+        if (iloSuccess) {
+            (bool os, ) = payable(owner()).call{value: address(this).balance}(
+                ""
+            );
+            require(os);
+        }
+    }
 }
